6b76f1805572959cee3b390c67ff4ebda7af3edd
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-07-14 00:45:26 +0430

    Convert class component to function component
    using Hooks

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,44 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { userService } from "../services/user.service";
+import { authenticationService } from "../services/authentication.service";
 
-import { userService } from '../services/user.service'
-import { authenticationService } from '../services/authentication.service'
+//class HomePage extends React.Component {
+function HomePage() {
+  // constructor(props) {
+  //     super(props);
 
+  //     this.state = {
+  //         currentUser: authenticationService.currentUserValue,
+  //         userFromApi: null
+  //     };
+  // }
+  const [currentUser, setCurrentUser] = useState(
+    authenticationService.currentUserValue
+  );
+  const [userFromApi, setUserFromApi] = useState(null);
 
+  // componentDidMount() {
+  //     const { currentUser } = this.state;
+  //     userService.getById(currentUser.id).then(userFromApi => this.setState({ userFromApi }));
+  // }
 
-class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
+  // useEffect(() => {
+  //   setCurrentUser(authenticationService.currentUserValue);
+  //   userService
+  //     .getById(currentUser.id)
+  //     .then((userFromApi) => setUserFromApi(userFromApi));
+  // });
 
-        this.state = {
-            currentUser: authenticationService.currentUserValue,
-            userFromApi: null
-        };
+  useEffect(() => {
+    const fetchState = {
+      cancel: false,
+    };
+    fetchUser(fetchState);
+    return () => {
+      fetchState.cancel = true;
+    };
+  }, []);
+
+  async function fetchUser(fetchState) {
+    const currentUser = authenticationService.currentUserValue;
+    if (fetchState.cancel) {
+      return null;
     }
+    setCurrentUser(currentUser);
+    userService
+      .getById(currentUser.id)
+      .then((userFromApi) => setUserFromApi(userFromApi));
+  }
 
-    componentDidMount() {
-        const { currentUser } = this.state;
-        userService.getById(currentUser.id).then(userFromApi => this.setState({ userFromApi }));
-    }
-
-    render() {
-        const { currentUser, userFromApi } = this.state;
-        return (
-            <div>
-                <h1>Home</h1>
-                <p>You're logged in with React & JWT!!</p>
-                <p>Your role is: <strong>{currentUser.role}</strong>.</p>
-                <p>This page can be accessed by all authenticated users.</p>
-                <div>
-                    Current user from secure api end point:
-                    {userFromApi &&
-                        <ul>
-                            <li>{userFromApi.firstName} {userFromApi.lastName}</li>
-                        </ul>
-                    }
-                </div>
-            </div>
-        );
-    }
+  //render() {
+  //const { currentUser, userFromApi } = this.state;
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Youre logged in with React & JWT!!</p>
+      <p>
+        Your role is:
+        <strong>{currentUser.role}</strong>.
+      </p>
+      <p>This page can be accessed by all authenticated users.</p>
+      <div>
+        Current user from secure api end point:
+        {userFromApi && (
+          <ul>
+            <li>
+              {userFromApi.firstName} {userFromApi.lastName}
+            </li>
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
-
+//}
 export { HomePage };
